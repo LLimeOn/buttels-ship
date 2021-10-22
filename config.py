b8f67ba2c1fad420907f870ae3c0abb7ee239f94
LLimeOn <78560285+LLimeOn@users.noreply.github.com>
2021-10-22 22:08:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import os
 f = requests.get("http://ifconfig.me/ip").text
 print(f)
 
-IP = f'{f}'
+# IP = f'{f}'
+IP = f'0.0.0.0'
+
 PORT = os.environ.get('PORTCH')
 
 SALT = 'SaltySaltySalt1'  # ur salt
